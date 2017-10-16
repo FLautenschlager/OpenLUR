2802c335e78a4a62d7427dab397bd41e875b0297
Michael Steininger <steininger@informatik.uni-wuerzburg.de>
2017-10-16 17:03:23 +0200

    implemented other performance metrics

--- Hasenfratz_r.py
+++ Hasenfratz_r.py
@@ -8,7 +8,8 @@ from rpy2.robjects import FloatVector, pandas2ri
 from rpy2.robjects.packages import importr
 
 # Load data
-pm_ha = sio.loadmat(paths.extdatadir + 'pm_ha_ext_01042012_30062012.mat')['pm_ha']
+pm_ha = sio.loadmat(paths.extdatadir +
+                    'pm_ha_ext_01042012_30062012.mat')['pm_ha']
 
 # mgcv is the R package with the GAM implementation
 mgcv = importr('mgcv')
@@ -19,13 +20,16 @@ stats = importr('stats')
 pandas2ri.activate()
 
 # Prepare data
-data_1 = pd.DataFrame(pm_ha[:,:3])
-data_2 = pd.DataFrame(pm_ha[:,7:])
+data_1 = pd.DataFrame(pm_ha[:, :3])
+data_2 = pd.DataFrame(pm_ha[:, 7:])
 calib_data = pd.concat([data_1, data_2], axis=1)
-calib_data.columns = ["x","y","pm_measurement","population","industry","floorlevel","heating","elevation","streetsize","signaldist","streetdist","slope","expo","traffic","streetdist_m","streetdist_l","trafficdist_l","trafficdist_h","traffic_tot"]
+calib_data.columns = ["x", "y", "pm_measurement", "population", "industry", "floorlevel", "heating", "elevation", "streetsize",
+                      "signaldist", "streetdist", "slope", "expo", "traffic", "streetdist_m", "streetdist_l", "trafficdist_l", "trafficdist_h", "traffic_tot"]
 
-model_var = pd.DataFrame(sio.loadmat(paths.rdir + 'model_ha_variables.mat')['model_variables'])
-model_var.columns = ["x","y","population","industry","floorlevel","heating","elevation","streetsize","signaldist","streetdist","slope","expo","traffic","streetdist_m","streetdist_l","trafficdist_l","trafficdist_h","traffic_tot"]
+model_var = pd.DataFrame(sio.loadmat(
+    paths.rdir + 'model_ha_variables.mat')['model_variables'])
+model_var.columns = ["x", "y", "population", "industry", "floorlevel", "heating", "elevation", "streetsize", "signaldist",
+                     "streetdist", "slope", "expo", "traffic", "streetdist_m", "streetdist_l", "trafficdist_l", "trafficdist_h", "traffic_tot"]
 
 # Select test and training dataset for 10 fold cross validation
 kf = KFold(n_splits=10, shuffle=True)
@@ -51,8 +55,7 @@ for train_index_calib, test_index_calib in kf.split(calib_data):
     ind_test_calib = test_calib_data.set_index(ind_keys).index
     ind_model_var = model_var.set_index(ind_keys).index
 
-    test_data = model_var[~ind_model_var.isin(ind_train_calib)]
-
+    test_model_var = model_var[~ind_model_var.isin(ind_train_calib)]
 
     # This is the formula for the GAM
     # From https://stat.ethz.ch/R-manual/R-devel/library/mgcv/html/smooth.terms.html:
@@ -60,7 +63,10 @@ for train_index_calib, test_index_calib in kf.split(calib_data):
     # "bs" is the basis of the used smooth class
     # "cr" declares a cubic spline basis
     # "k" defines the dimension of the basis (upper limit on degrees of freedom)
-    formula = robjects.Formula('pm_measurement~s(industry,bs="cr",k=3)+s(floorlevel,bs="cr",k=3)+s(elevation,bs="cr",k=3)+s(slope,bs="cr",k=3)+s(expo,bs="cr",k=3)+streetsize+s(traffic_tot,bs="cr",k=3)+s(streetdist_l,bs="cr",k=3)')
+    formula = robjects.Formula('pm_measurement~s(industry,bs="cr",k=3)' +
+        '+s(floorlevel,bs="cr",k=3)+s(elevation,bs="cr",k=3)' +
+        '+s(slope,bs="cr",k=3)+s(expo,bs="cr",k=3)+streetsize' +
+        '+s(traffic_tot,bs="cr",k=3)+s(streetdist_l,bs="cr",k=3)')
     # Hasenfratz uses a Gamma distribution with a logarithmic link
     family = stats.Gamma(link='log')
 
@@ -69,30 +75,43 @@ for train_index_calib, test_index_calib in kf.split(calib_data):
     su = base.summary(model)
 
     # Predict the test data
-    pred_data = stats.predict(model, newdata=test_data, type='response')
-    test_data_predictions = test_data.assign(prediction=pred_data)
+    pred_data = stats.predict(model, newdata=test_model_var, type='response')
+    test_model_var_predictions = test_model_var.assign(prediction=pred_data)
+    ind_test_model_var_pred = test_model_var_predictions.set_index(
+        ind_keys).index
+
+    # Create a DataFrame where it is easily possible to compare measurements and
+    # predictions
+    test_measure_predict = test_calib_data.merge(
+        test_model_var_predictions, how='inner', on=['x', 'y']
+        )[['x', 'y', 'pm_measurement', 'prediction']]
     # Check how large the error is with the remaining 10% of data
-    error_model = test_calib_data['pm_measurement']-test_data_predictions['prediction']
+    error_model = test_measure_predict['pm_measurement'] - \
+        test_measure_predict['prediction']
     # Drop all NaN's
     error_model = error_model.dropna()
-    print(error_model)
     # Calculate Root-mean-square error model
     rmse_model.append(np.sqrt(np.mean(error_model**2)))
-    # rsq_model <- c(rsq_model, su$r.sq)
-    # devexpl_model <- c(devexpl_model, su$dev.expl*100)
-    # fac2_ind <- which(calib_data[-ind,3]/pred_data<=2 & calib_data[-ind,3]/pred_data>=0.5)
-    # fac2_model <- c(fac2_model, length(fac2_ind)/length(ind_var)*100)
-    # # calculate R2 between predicted and measured concentration
-    # lt1 <- lm(calib_data[-ind,3]~pred_data)
-    # rsqval_model <- c(rsqval_model, summary(lt1)$r.squared)
+    # Get R² from summary
+    rsq_model.append(su.rx2('r.sq'))
+    devexpl_model.append(su.rx2('dev.expl'))
+    # Calculate Factor of 2
+    fac2_ind = test_measure_predict['pm_measurement'] / \
+        test_measure_predict['prediction']
+    fac2_ind = fac2_ind[(fac2_ind <= 2) & (fac2_ind >= 0.5)].dropna()
+    fac2_model.append(len(fac2_ind) / len(test_measure_predict['pm_measurement']) * 100)
 
-    # print(assign_test)
-    # print(pred_data)
-    # print(len(pred_data))
-    # print(test_calib_data)
-    # print(rmse_model)
-    # print(su)
-    # input()
+    # calculate R2 between predicted and measured concentration
+    r2val_formula = robjects.Formula('measurements~predictions')
+    r2val_env = r2val_formula.environment
+    r2val_env['measurements'] = test_measure_predict['pm_measurement']
+    r2val_env['predictions'] = test_measure_predict['prediction']
+    lt1 = stats.lm(r2val_formula)
+    rsqval_model.append(base.summary(lt1).rx2('r.squared'))
 
 
-print('Root-mean-square error:', np.mean(rmse_model))
+print('Root-mean-square error:', np.mean(rmse_model), 'particles/cm^3')
+print('R2:', np.mean(rsq_model))
+print('R2-val:', np.mean(rsqval_model))
+print('DevExpl:', np.mean(devexpl_model) * 100)
+print('FAC2:', np.mean(fac2_model))
