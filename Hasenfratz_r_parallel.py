9d77bcdbef8337fa8b33a223ff57fe1be77fb31d
Michael Steininger <steininger@informatik.uni-wuerzburg.de>
2017-11-10 18:54:13 +0100

    added batch cross validation for hasenfratz

--- Hasenfratz_r_parallel.py
+++ Hasenfratz_r_parallel.py
@@ -11,6 +11,7 @@ import pickle
 import rpy2.robjects as robjects
 from rpy2.robjects import FloatVector, pandas2ri
 from rpy2.robjects.packages import importr
+from rpy2.rinterface import RRuntimeError
 
 FEATURE_COLS = ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
                 'streetsize', 'traffic_tot', 'streetdist_l']
@@ -30,8 +31,8 @@ def build_formula_string(feature_cols):
     return formula[:-1]
 
 
-def calculate_gam(inputs, feature_cols):
-    train_calib_data, test_calib_data, test_model_var = inputs
+def calculate_gam(inputs):
+    train_calib_data, test_calib_data, test_model_var, feature_cols = inputs
 
     # mgcv is the R package with the GAM implementation
     mgcv = importr('mgcv')
@@ -95,7 +96,7 @@ def calculate_gam(inputs, feature_cols):
     # Return metrics and predicted values
     return rmse, rsq, rsqval, devexpl, fac2, test_measure_predict
 
-def cross_validate(calib_data, model_var, feature_cols=FEATURE_COLS):
+def cross_validate(calib_data, model_var, jobs, feature_cols=FEATURE_COLS, repeat=40,):
     # Select test and training dataset for 10 fold cross validation
     kf = KFold(n_splits=10, shuffle=True)
 
@@ -106,11 +107,11 @@ def cross_validate(calib_data, model_var, feature_cols=FEATURE_COLS):
     rsqval_model = []
 
     gam_inputs = []
-    pool = Pool(processes=int(args.jobs))
+    pool = Pool(processes=int(jobs))
 
     # Hasenfratz does the 10 fold cross validation 40 times to get a better coverage
     # of the model variables
-    for _ in range(40):
+    for _ in range(repeat):
         for train_index_calib, test_index_calib in kf.split(calib_data):
             train_calib_data = calib_data.iloc[train_index_calib]
             test_calib_data = calib_data.iloc[test_index_calib]
@@ -126,12 +127,29 @@ def cross_validate(calib_data, model_var, feature_cols=FEATURE_COLS):
             test_model_var = model_var[~ind_model_var.isin(ind_train_calib)]
 
             # First gather all the inputs for each GAM calculation in a list
-            gam_inputs.append((train_calib_data, test_calib_data, test_model_var))
+            gam_inputs.append((train_calib_data, test_calib_data, test_model_var, feature_cols))
 
 
-    # Add all the GAM calculations with their respective inputs into the Pool
-    # returns rmse, rsq, rsqval, devexpl, fac2
-    results = pd.DataFrame(pool.map(calculate_gam, gam_inputs))
+    try:
+        # Add all the GAM calculations with their respective inputs into the Pool
+        # returns rmse, rsq, rsqval, devexpl, fac2
+        results = pd.DataFrame(pool.map(calculate_gam, gam_inputs))
+    except RRuntimeError:
+        pool.close()
+        print('R Error')
+        return {
+            'mean-batch-rmse': None,
+            'rmse': None,
+            'mean-batch-rsq': None,
+            'mean-batch-rsq-val': None,
+            'rsq-val': None,
+            'adj-rsq-val': None,
+            'devexpl': None,
+            'fac2': None,
+            'predictions': None}
+
+
+    pool.close()
 
     results.columns = ['rmse', 'rsq', 'rsqval', 'devexpl', 'fac2', 'predictions']
 
@@ -159,7 +177,7 @@ def cross_validate(calib_data, model_var, feature_cols=FEATURE_COLS):
     # Calculate adjusted R²: rsq-(1-rsq)*p/(n-p-1)
     p = len(feature_cols)
     n = len(calib_data)
-    ajd_skl_rsq_val = skl_rsq_val-(1-skl_rsq_val)*p/(n-p-1)
+    adj_skl_rsq_val = skl_rsq_val-(1-skl_rsq_val)*p/(n-p-1)
 
 
     print('Root-mean-square error:', np.mean(rmse_model), 'particles/cm^3')
@@ -187,9 +205,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-j', '--jobs', default=cpu_count(), type=int, help='Specifies the number of jobs to run simultaneously.')
     parser.add_argument('-c', '--cross', action='store_true',
-                        help='Train and evaluate neural network with 10-fold cross validation.')
+                        help='Train and evaluate with 10-fold cross validation.')
     parser.add_argument('-b', '--batch_cross', action='store_true',
-                        help='Train and evaluate batches of neural networks with 10-fold cross validation.')
+                        help='Train and evaluate batches with 10-fold cross validation.')
     args = parser.parse_args()
 
     if args.cross is True:
@@ -209,13 +227,98 @@ if __name__ == '__main__':
         model_var.columns = ['x', 'y', 'population', 'industry', 'floorlevel', 'heating', 'elevation', 'streetsize', 'signaldist',
                              'streetdist', 'slope', 'expo', 'traffic', 'streetdist_m', 'streetdist_l', 'trafficdist_l', 'trafficdist_h', 'traffic_tot']
 
-        cross_validate(calib_data, model_var)
+        cross_validate(calib_data, model_var, args.jobs)
 
-    # run_info = {
-    #     'source': 'Hasenfratz',
-    #     'feature_cols': FEATURE_COLS,
-    #     'tiles': len(calib_data),
-    #     'timeframe': '01072012_31092012'
-    # }
+    if args.batch_cross is True:
+        calib_files = [
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_1200.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_1100.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_1000.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_900.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_800.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_700.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_600.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_500.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_400.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_300.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_250.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_200.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_170.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_140.csv',
+            paths.extdatadir + 'pm_01072012_31092012_filtered_ha_110.csv'#,
+            # paths.extdatadir + 'pm_01072012_31092012_filtered_ha_80.csv',
+            # paths.extdatadir + 'pm_01072012_31092012_filtered_ha_50.csv',
+            # paths.extdatadir + 'pm_01072012_31092012_filtered_ha_20.csv'
+        ]
+        feature_cols_list = [
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l'],
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l', 'population'],
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+            #     'heating'],
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+            #     'heating', 'signaldist'],
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+            #     'heating', 'signaldist', 'streetdist'],
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+            #     'heating', 'signaldist', 'streetdist', 'traffic'],
+            # ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+            #     'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+            #     'heating', 'signaldist', 'streetdist', 'traffic',
+            #     'streetdist_m'],
+            ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+                'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+                'heating', 'signaldist', 'streetdist', 'traffic',
+                'streetdist_m', 'trafficdist_l'],
+            ['industry', 'floorlevel', 'elevation', 'slope', 'expo',
+                'streetsize', 'traffic_tot', 'streetdist_l', 'population',
+                'heating', 'signaldist', 'streetdist', 'traffic',
+                'streetdist_m', 'trafficdist_l', 'trafficdist_h']
+        ]
 
-    print(build_formula_string(FEATURE_COLS))
+        results_list = []
+
+        for feature_cols in feature_cols_list:
+            for calib_file in calib_files:
+                # Load new data set from file
+                calib_data = pd.read_csv(calib_file)
+
+                # Load model_var
+                model_var = pd.DataFrame(sio.loadmat(
+                    paths.rdir + 'model_ha_variables.mat')['model_variables'])
+                model_var.columns = ['x', 'y', 'population', 'industry', 'floorlevel', 'heating', 'elevation', 'streetsize', 'signaldist',
+                                     'streetdist', 'slope', 'expo', 'traffic', 'streetdist_m', 'streetdist_l', 'trafficdist_l', 'trafficdist_h', 'traffic_tot']
+
+                run_info = {
+                    'source': 'Hasenfratz',
+                    'feature_cols': feature_cols,
+                    'tiles': len(calib_data),
+                    'timeframe': '01072012_31092012'
+                }
+
+                print('Next CV:', run_info)
+
+                # Do 10-fold cross validation on new data set
+                results = cross_validate(calib_data, model_var, args.jobs, feature_cols=feature_cols, repeat=1)
+
+                # Merge run information with results
+                results = {**run_info, **results}
+                # Calculate adjusted R²: rsq-(1-rsq)*p/(n-p-1)
+                # rsq = results['rsq-val']
+                # p = len(feature_cols)
+                # n = results['tiles']
+                # results['ajd-rsq-val'] = rsq-(1-rsq)*p/(n-p-1)
+                # Remove predictions since i can't have a table in a table when
+                # outputting to csv
+                del results['predictions']
+
+                # Store results in a list
+                results_list.append(results)
+
+        results_list_df = pd.DataFrame(results_list)
+        results_list_df.to_csv('tiles_features_batchcv_hasenfratz.csv', mode='a', index=False)
