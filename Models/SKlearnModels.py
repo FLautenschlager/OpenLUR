8a164ffcec889e8967fc5d9ff8327a32afdaebdf
Florian Lautenschlager <lautenschlager@informatik.uni-wuerzburg.de>
2018-02-21 12:08:15 +0100

    RF, Wrapper

---
+++ Models/SKlearnModels.py
@@ -0,0 +1,9 @@
+from Models import SklearnWrapper
+from sklearn.linear_model import SGDRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+def getRF(njobs, niter=40, verbosity=0):
+	return SklearnWrapper(njobs, RandomForestRegressor, niter, verbosity)
+
+def getSGD(njobs, niter=40, verbosity=0):
+	return SklearnWrapper(njobs, SGDRegressor, niter, verbosity)
--- runner.py
+++ runner.py
@@ -10,6 +10,7 @@ from Models.AutoSKLearn import AutoSKLearn
 from Models.AutoSKLearn_external import AutoRegressor
 from utils import paths
 from Models import SGD
+from Models.SKlearnModels import *
 
 # from rpy2.rinterface import RRuntimeError
 
@@ -105,7 +106,10 @@ if __name__ == '__main__':
 		pickle.dump(result, open(paths.autosklearn + "season{}_Features_{}.p".format(args.seasonNumber, feat), 'wb'))
 	elif args.model==4:
 		print("start SVD")
-		model = SGD.SGD(njobs=njobs, niter=iterations, verbosity=2)
+		model = getSGD(njobs=njobs, niter=iterations, verbosity=2)
+		model.test_model(data, feat_columns, target)
+	elif args.model==5:
+		model = getRF(njobs=njobs, niter=iterations, verbosity=2)
 		model.test_model(data, feat_columns, target)
 
 
