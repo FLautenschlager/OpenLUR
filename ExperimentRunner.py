2f97bd6a25511945a21863ba67a3437f53d68452
Florian Lautenschlager <lautenschlager@informatik.uni-wuerzburg.de>
2019-05-20 17:22:05 +0200

    building datasets in iteration loop

--- ExperimentRunner.py
+++ ExperimentRunner.py
@@ -10,6 +10,21 @@ from utils.DataLoader import Dataset
 from utils.MyPool import MyPool
 from utils.color import Color
 
+features = [
+    "os",
+    "laei_small",
+    "laei",
+    "both"
+]
+feature_type=features[3]
+
+modelnames = [
+    "AutoML",
+    "Random_Forest_random_search",
+    "Random_Forest_Standard",
+    # "GAM"
+]
+iterations = 4
 
 def run_londondata_mapper(args):
     model, iterations = args
@@ -38,39 +53,46 @@ def run_on_both(model, iterations=2, filename=None, season=1):
     x_train_laei, y_train_laei, x_test_laei, y_test_laei = Dataset.laeiOSM()
     x_train_os, y_train_os, x_test_os, y_test_os = Dataset.OpenSenseOSM(season)
 
-    # select 180 points from laei:
-    idx = np.random.choice(x_train_laei.shape[0], 180, replace=False)
-    x_train_laei_split = x_train_laei[idx,:]
-    y_train_laei_split = y_train_laei[idx]
-
-    # select 20 points from laei (test):
-    idx = np.random.choice(x_test_laei.shape[0], 20, replace=False)
-    x_test_laei_split = x_test_laei[idx, :]
-    y_test_laei_split = y_test_laei[idx]
-
-    # split os in 180/20
-    x_train_os_split, x_test_os_split, y_train_os_split, y_test_os_split = train_test_split(x_train_os, y_train_os, test_size=0.1, random_state = 42)
-
-    # concatenate os and laei data
-    x_train = np.concatenate((x_train_laei_split, x_train_os_split), axis=0)
-    x_test = np.concatenate((x_test_laei_split, x_test_os_split), axis=0)
-    y_train = np.concatenate((y_train_laei_split, y_train_os_split), axis=0)
-    y_test = np.concatenate((y_test_laei_split, y_test_os_split), axis=0)
-
-
-    x_train = x_train_laei_split
-    y_train = y_train_laei_split
-    x_test = x_test_laei_split
-    y_test = y_test_laei_split
-
-    print(x_train.shape)
-    print(y_train.shape)
-
-    print("Start model {}".format(model))
-
+    print("Start model {} on {}".format(model, feature_type))
     starttime = time.time()
     results = []
     for i in range(iterations):
+
+        # select 180 points from laei:
+        idx = np.random.choice(x_train_laei.shape[0], 180, replace=False)
+        x_train_laei_split = x_train_laei[idx,:]
+        y_train_laei_split = y_train_laei[idx]
+
+        # select 20 points from laei (test):
+        idx = np.random.choice(x_test_laei.shape[0], 20, replace=False)
+        x_test_laei_split = x_test_laei[idx, :]
+        y_test_laei_split = y_test_laei[idx]
+
+        # split os in 180/20
+        x_train_os_split, x_test_os_split, y_train_os_split, y_test_os_split = train_test_split(x_train_os, y_train_os, test_size=0.1, random_state = 42)
+
+        if feature_type=="both":
+            # concatenate os and laei data
+            x_train = np.concatenate((x_train_laei_split, x_train_os_split), axis=0)
+            x_test = np.concatenate((x_test_laei_split, x_test_os_split), axis=0)
+            y_train = np.concatenate((y_train_laei_split, y_train_os_split), axis=0)
+            y_test = np.concatenate((y_test_laei_split, y_test_os_split), axis=0)
+        elif feature_type=="laei_small":
+            x_train = x_train_laei_split
+            y_train = y_train_laei_split
+            x_test = x_test_laei_split
+            y_test = y_test_laei_split
+        elif feature_type=="laei":
+            x_train = x_train_laei
+            y_train = y_train_laei
+            x_test = x_test_laei
+            y_test = y_test_laei
+        elif feature_type=="os":
+            x_train = x_train_os_split
+            y_train = y_train_os_split
+            x_test = x_test_os_split
+            y_test = y_test_os_split
+
         results.append(run_regression(model, x_train, y_train, x_test, y_test))
 
     results = pd.concat(results, ignore_index=True)
@@ -86,15 +108,9 @@ def run_on_both(model, iterations=2, filename=None, season=1):
 
 if __name__=="__main__":
 
-    modelnames = [
-        "AutoML",
-        "Random_Forest_random_search",
-        "Random_Forest_Standard",
-        #"GAM"
-        ]
-    iterations = 4
+
 
     for model in modelnames:
          #run_londondata(model, iterations=iterations, filename="output/{}_longrun.p".format(model))
-         run_on_both(model, iterations=iterations, filename="output/{}_train_laei_small_{}_iterations.p".format(model, iterations))
+         run_on_both(model, iterations=iterations, filename="output/{}_train_{}_{}_iterations.p".format(model, feature_type, iterations))
 
